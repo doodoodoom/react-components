2ef94bdd6f1cfddd7c1214d30b1ab95b9daba88b
doodoodoom <madridmatt17@gmail.com>
2019-01-16 16:52:07 -0800

    Implement GroceryList and GroceryListItem in component and class component styles

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // TODO
+var GroceryList = (props) => (
+  <div>
+    GROCERIES
+    <ul>
+      {props.items.map(item =>
+        <GroceryListItem item={item} />
+      )}
+    </ul>
+  </div>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      'font-weight': this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseOver={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+};
+
+// {this.onListItemHover.bind(this)}
+
+
+ReactDOM.render(
+  <GroceryList items={['milk', 'eggs', 'bagels', 'cheese', 'ice cream']}/>,
+  document.getElementById('app')
+);
